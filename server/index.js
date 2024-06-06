4c41cddceae7827737fa6118bd8c54fc83b8f73e
shaddu007 <shadan.alam.ece24@heritageit.edu.in>
2024-06-06 07:31:19 +0530

    path added

--- server/index.js
+++ server/index.js
@@ -31,3 +31,14 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
   .catch((err) => console.log(`${err} did not connect`));
+
+  //new added
+  const path = require('path');
+  
+  app.use(express.static(path.join(__dirname, 'build')));
+  
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+  
+  app.listen(3001);
