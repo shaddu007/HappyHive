d194a294206809d4832a4ab3756589d9ffb64dfa
shaddu007 <shadan.alam.ece24@heritageit.edu.in>
2024-06-06 07:17:11 +0530

    full stack

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,14 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+      Feel at home, no matter where you are <br /> Live in the moment 
+      and cherish the memories you create
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import "../styles/Login.scss";
+import { setLogin } from "../redux/state";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(""); // State to store error messages
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3001/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const loggedIn = await response.json();
+
+      if (response.ok) {
+        dispatch(
+          setLogin({
+            user: loggedIn.user,
+            token: loggedIn.token,
+          })
+        );
+        navigate("/");
+      } else {
+        // Handle specific error messages
+        if (response.status === 409) {
+          setError("User doesn't exist!");
+        } else if (response.status === 400) {
+          setError("Invalid Credentials!");
+        } else {
+          setError("Login failed. Please try again.");
+        }
+      }
+    } catch (err) {
+      console.log("Login failed", err.message);
+      setError("An error occurred. Please try again later.");
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="login_content">
+        <form className="login_content_form" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          {error && <p className="error">{error}</p>} {/* Display error message */}
+          <button type="submit">LOG IN</button>
+        </form>
+        <a href="/register">Don't have an account? Sign In Here</a>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
